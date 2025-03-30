6dff16b3ebbc241a4a0e302664856a9f5a195e6c
renou <2193239205@qq.com>
2025-03-30 18:22:50 +0800

    doc: GraphQL

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,66 +1,72 @@
-import { defineConfig } from "vitepress";
+import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Renouc",
-  description: "积累知识碎片",
+  title: 'Renouc',
+  description: '积累知识碎片',
   cleanUrls: true,
-  srcDir: "src",
-  head: [["link", { rel: "icon", href: "/logo.svg" }]],
+  srcDir: 'src',
+  head: [['link', { rel: 'icon', href: '/logo.svg' }]],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    logo: "/logo.png",
+    logo: '/logo.png',
     nav: [
       {
-        text: "前端知识",
+        text: '前端知识',
         items: [
           {
-            text: "React",
-            link: "/frontend/react",
+            text: 'React',
+            link: '/frontend/react',
           },
           {
-            text: "JavaScript",
-            link: "/frontend/javascript",
+            text: 'JavaScript',
+            link: '/frontend/javascript',
           },
           {
-            text: "Next.js",
-            link: "/frontend/next",
+            text: 'Next.js',
+            link: '/frontend/next',
           },
         ],
       },
       {
-        text: "DevOps",
-        link: "/devops",
+        text: 'DevOps',
+        link: '/devops',
       },
-      { text: "知识碎片", link: "/fragment" },
+      { text: '知识碎片', link: '/fragment' },
     ],
 
     sidebar: {
-      "/frontend/": [
+      '/frontend/': [
         {
-          text: "JavaScript",
-          items: [{ text: "this 的绑定", link: "/frontend/javascript/this" }],
+          text: 'JavaScript',
+          items: [{ text: 'this 的绑定', link: '/frontend/javascript/this' }],
         },
         {
-          text: "React",
-          items: [{ text: "React 的基本使用", link: "/frontend/react/base" }],
+          text: 'React',
+          items: [{ text: 'React 的基本使用', link: '/frontend/react/base' }],
         },
         {
-          text: "Next.js",
+          text: 'Next.js',
           items: [
-            { text: "Next.js 渲染模式", link: "/frontend/next/render" },
-            { text: "Next.js 中间件", link: "/frontend/next/middleware" },
+            { text: 'Next.js 渲染模式', link: '/frontend/next/render' },
+            { text: 'Next.js 中间件', link: '/frontend/next/middleware' },
           ],
         },
       ],
-      "/devops/": [
+      '/devops/': [
         {
-          text: "Docker",
-          items: [{ text: "Docker 的基本使用", link: "/devops/docker" }],
+          text: 'Docker',
+          items: [{ text: 'Docker 的基本使用', link: '/devops/docker' }],
+        },
+      ],
+      '/fragment/': [
+        {
+          text: 'GraphQL',
+          link: '/fragment/graphql',
         },
       ],
     },
 
-    socialLinks: [{ icon: "github", link: "https://github.com/Renouc" }],
+    socialLinks: [{ icon: 'github', link: 'https://github.com/Renouc' }],
   },
-});
+})
---
+++ src/fragment/graphql.md
@@ -0,0 +1,9 @@
+# GraphQL
+
+GraphQL 是 API 查询语言，支持客户端按需获取数据。
+
+后端：`express-graphql`
+
+前端：`@apollo/client`
+
+示例：https://github.com/Renouc/node-demo/tree/main/graphql-demo
